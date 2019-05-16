f67f39b695901f51e4e4e6278c17cdccadcb63ed
Glitch (request-header-parser-microservice-wpdas) <none>
2019-05-16 04:16:36 +0000

    🙌🗺 Checkpoint ./server.js:4469722/2007

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 const cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/api/whoami', (req, res) => {
-  const ip = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
+  const ip =
+    req.headers['x-forwarded-for'].split(', ').pop() ||
+    req.connection.remoteAddress;
   const languages = req.acceptsLanguages();
   const userAgent = req.get('user-agent');
-  
+
   res.json({
     ipaddress: ip.split(',')[0],
     language: languages.join(','),
     software: userAgent
   });
 });
-
-
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
