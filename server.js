40f0ccc1e461f2d70249b3257c9bb475be1acae7
Glitch (veil-paste) <none>
2019-05-16 04:01:38 +0000

    Exporting to Wpdas/request-header-parser-microservice ./server.js:4469722/1474

--- server.js
+++ server.js
@@ -10,15 +10,23 @@ const app = express();
 const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.get('/api/whoami', (req, res) => {
-  const IP = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
+  const ip = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
   const languages = req.acceptsLanguages();
   const userAgent = req.get('user-agent');
   
   res.json({
-    ipaddress: IP.split(',')[0],
+    ipaddress: ip.split(',')[0],
     language: languages.join(','),
-    software: userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'))
+    software: userAgent
   });
 });
 
