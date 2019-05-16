760e2003abff404b7c790c91cc40bd1f376f63af
Glitch (veil-paste) <none>
2019-05-16 03:43:37 +0000

    🖼🌲 Checkpoint ./server.js:4469722/98

--- server.js
+++ server.js
@@ -2,30 +2,30 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.get('/api/whoami', (req, res) => {
-  var IP = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
-  var languages = req.acceptsLanguages();
-  var userAgent = req.get('user-agent');
+  const IP = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
+  const languages = req.acceptsLanguages();
+  const userAgent = req.get('user-agent');
   
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
+  res.json({
     ipaddress: IP,
     language: languages[0],
     software: userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'))
-  }));
+  });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
