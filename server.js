03078d5550cae1450f14007d5efec24591caedc3
Glitch (veil-paste) <none>
2019-05-16 03:32:37 +0000

    ⛰🚛 Checkpoint ./server.js:4469722/812

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/whoami', (req, res) => {
+  var IP = req.headers['x-forwarded-for'].split(', ').pop() || req.connection.remoteAddress;
+  var languages = req.acceptsLanguages();
+  var userAgent = req.get('user-agent');
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    ipaddress: IP,
+    language: languages[0],
+    software: userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'))
+  }));
 });
 
 
